575b7100053309a5f47e3f8f1180ab4f872b6afb
chillbroskie <64366196+chillbroskie@users.noreply.github.com>
2022-08-01 17:26:37 -0400

    gallery halkf done

--- src/container/Gallery/Gallery.jsx
+++ src/container/Gallery/Gallery.jsx
@@ -1,11 +1,43 @@
-import React from 'react';
+import React from "react";
+import {
+  BsInstagram,
+  BsArrowLeftShort,
+  BsArrowRightShort,
+} from "react-icons/bs";
+import { useRef } from "react";
+import { SubHeading } from "../../components";
+import { images } from "../../constants";
+import "./Gallery.css";
 
-import './Gallery.css';
+const Gallery = () => {
+  const scrollRef = useRef(null);
+  const scroll = (direction) => {
+    const { current } = scrollRef;
 
-const Gallery = () => (
-  <div>
-    Gallery
-  </div>
-);
+    if (direction === "left") {
+      current.scrollLeft -= 300;
+    } else {
+      current.scrollLeft += 300;
+    }
+  };
 
+  return (
+    <div className="app__gallery flex__center">
+      <div className="app__gallery-content">
+        <SubHeading title="Instagram" />
+        <h1 className="headtext__cormorant">Photo Gallery</h1>
+        <p
+          className="p__opensans"
+          style={{ color: "#AAAAAA", marginTop: "2rem" }}
+        >
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Volutpat
+          mattis ipsum turpis elit elit scelerisque egestas mu.
+        </p>
+        <button type="button" className="custom__button">
+          View More
+        </button>
+      </div>
+    </div>
+  );
+};
 export default Gallery;
--- src/container/Intro/Intro.jsx
+++ src/container/Intro/Intro.jsx
@@ -1,11 +1,45 @@
-import React from 'react';
+import React from "react";
+import { BsFillPlayFill, BsPauseFill } from "react-icons/bs";
+import { meal } from "../../constants";
+import { useState, useRef } from "react";
 
-import './Intro.css';
+import "./Intro.css";
 
-const Intro = () => (
-  <div>
-    Intro
-  </div>
-);
+const Intro = () => {
+  const [playvideo, setPlayvideo] = useState(false);
+  const videoRef = useRef(null);
+
+  return (
+    <div className="app__video">
+      <video
+        ref={videoRef}
+        src={meal}
+        type="video/mp4"
+        loop
+        controls={false}
+        muted
+      />
+      <div className="app__video-overlay flex__center">
+        <div
+          className="app__video-overlay_circle flex__center"
+          onClick={() => {
+            setPlayvideo(!playvideo);
+            if (playvideo) {
+              videoRef.current.play();
+            } else {
+              videoRef.current.pause();
+            }
+          }}
+        >
+          {playvideo ? (
+            <BsPauseFill color="#fff" fontSize={30} />
+          ) : (
+            <BsFillPlayFill color="#fff" fontSize={30} />
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Intro;
